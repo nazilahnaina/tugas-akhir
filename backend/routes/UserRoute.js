571bd4118a243afecf3a7b1961295de7b1f48eec
nazilahnaina <nazilahnaina@gmail.com>
2023-12-13 08:17:15 +0700

    tugas-akhir

---
+++ backend/routes/UserRoute.js
@@ -0,0 +1,18 @@
+import express from "express";
+import {
+    getUsers,
+    getUserByld,
+    createUser,
+    updateUser,
+    deleteUser
+} from "../controllers/UserController.js";
+
+const router = express.Router();
+
+router.get('/users', getUsers);
+router.get('/users/:id', getUserByld);
+router.post('/users', createUser);
+router.patch('/users/:id', updateUser);
+router.delete('/users/:id', deleteUser);
+
+export default router;
